1012a4cdfeff2d51f6d3ca910f36e8be49446013
Sternel <dvu.dima@mail.ru>
2025-01-05 10:12:59 +0700

    Fixed work with KTP

--- js/KTP.js
+++ js/KTP.js
@@ -1,10 +1,14 @@
+
 // Импорт КТП
 chrome.storage.local.get(['isEnabledImportKTP'], function(data) {
-    if (document.location.href.match("https://spo.ruobr.ru/study/ktp/*/") && data.isEnabledImportKTP == true) {
+    if (document.location.href.match("https:\/\/spo.ruobr.ru\/study\/ktp\/.*\/$") && data.isEnabledImportKTP == true) {
         const button = document.createElement("button");
         button.className = "btn btn-primary";
         button.innerHTML = "Импорт";
         button.onclick = function() {
+            // Блокируем сайт, кроме формы
+            document.body.style.pointerEvents = "none";
+            
             // Создаем форму
             let importDiv = document.createElement("div");
             importDiv.id = "ImportForm";
@@ -33,14 +37,28 @@ chrome.storage.local.get(['isEnabledImportKTP'], function(data) {
             buttonImport.onclick = function() {
                 importKTP(inputFile.files[0], inputDelimiter.value);
             }
-    
+            // Создаём кнопку Отмена
+            let cancelButton = document.createElement("button");
+            cancelButton.type = "submit";
+            cancelButton.className = "btn btn-primary";
+            cancelButton.innerHTML = "Отмена";
+            // Создаём блок для кнопок управления
+            let controls = document.createElement("div");
+            controls.appendChild(buttonImport);
+            controls.appendChild(cancelButton);
+
             // Добавляем форму в div
             labelDelimiter.appendChild(inputDelimiter);
             importDiv.appendChild(labelDelimiter);
             importDiv.appendChild(inputFile);
-            importDiv.appendChild(buttonImport);
+            importDiv.appendChild(controls);
             // Добавляем форму в документ
             document.body.appendChild(importDiv);
+            cancelButton.onclick = function() {
+                document.body.removeChild(importDiv);
+                document.body.style.pointerEvents = "auto";
+            }
+            importDiv.style.pointerEvents = "auto";
         }
         let controls = document.querySelectorAll(".controls div");
         controls[1].appendChild(button);
@@ -49,7 +67,7 @@ chrome.storage.local.get(['isEnabledImportKTP'], function(data) {
 
 // Экспорт КТП
 chrome.storage.local.get(['isEnabledExportKTP'], function(data) {
-    if (document.location.href.match("https://spo.ruobr.ru/study/ktp/*/") && data.isEnabledExportKTP == true) {
+    if (document.location.href.match("https:\/\/spo.ruobr.ru\/study\/ktp\/.*\/$") && data.isEnabledExportKTP == true) {
         const button = document.createElement("button");
         button.className = "btn btn-primary";
         button.innerHTML = "Экспорт";
@@ -62,7 +80,7 @@ chrome.storage.local.get(['isEnabledExportKTP'], function(data) {
 });
 // Очистить КТП
 chrome.storage.local.get(['isEnabledClearKTP'], function(data) {
-    if (document.location.href.match("https://spo.ruobr.ru/study/ktp/*/") && data.isEnabledClearKTP == true) {
+    if (document.location.href.match("https:\/\/spo.ruobr.ru\/study\/ktp\/.*\/$") && data.isEnabledClearKTP == true) {
         const button = document.createElement("button");
         button.className = "btn btn-primary";
         button.innerHTML = "Очистить";
@@ -90,15 +108,19 @@ function importKTP(file, delimiter) {
             if (data[0] != "") {
                 var xhr = new XMLHttpRequest();
                 str = "https://spo.ruobr.ru/study/ktp/" + window.location.href.split("/")[5] + "/add/"
-                xhr.open("POST", str, true);
+                xhr.open("POST", str, false);
                 xhr.setRequestHeader('Content-Type', 'application/json');
                 var request = `ktp_subject=${window.location.href.split("/")[5]}&order_num=${i - 1}&name=${data[0]}&duration=${data[1]}&text=${data[2]}&hw=&csrfmiddlewaretoken=${document.cookie.split(";")[0].split("=")[1]}`;
-                console.log(request);
+                xhr.onprogress = function(event) { // запускается периодически
+                    console.log(`Отправлено ${i}`);
+                };
                 xhr.send(request);
             }
         }
     }
-    location.reload(5);
+    setTimeout(function(){
+        location.reload();
+       },3000);
 }
 // Очистить КТП
 function clearKTP() {
@@ -111,23 +133,28 @@ function clearKTP() {
         // Получаем id строки
         let id = rows[i].getAttribute('id');
         // Получаем ссылку на удаление
-        let link = window.location.href + "delete/" + id + "/";
+        let link = "https://spo.ruobr.ru/study/ktp/" + window.location.href.split("/")[5] + "/delete/" + id + "/";
+        console.log(link);
         var formData = new FormData(document.forms.person);
         // добавить к пересылке ещё пару ключ - значение
         formData.append("csrfmiddlewaretoken", `${document.cookie.split(";")[0].split("=")[1]}`);
 
         // Отправляем POST запрос на удаление
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", link);
+        xhr.open("POST", link, false);
+
+        xhr.onprogress = function(event) { // запускается периодически
+            console.log(`Отправлено ${i}`);
+        };
         xhr.send(formData);
-        console.log(link);
+        
     }
     location.reload();
 }
 // Экспорт КТП в файл csv
 function exportKTP() {
     // Переменная для хранения данных в файл
-    let data = "";
+    let data = "Порядковый номер,Тема урока,Черновик,Количество часов,Содержание\n";
     // Получаем таблицу
     let table = document.querySelector("table");
     // Получаем все строки
@@ -139,14 +166,9 @@ function exportKTP() {
         // Получаем все ячейки
         let cells = row.querySelectorAll("td");
         // Проходимся по ячейкам
-        for (let j = 1; j < cells.length - 2; j++) {
+        for (let j = 2; j < cells.length - 2; j++) {
             if (cells[j].innerText != "—"){
-                if (j == 1) {
-                    // Получаем значение ячейки
-                    let value = cells[j].innerText.slice(2);
-                    // Добавляем значение в строку
-                    data += value + ",";
-                }else if (j == 3) {
+                if (j == 3) {
                     data += cells[j].innerHTML.match("Да").index != 0 ? "Да" : "Нет";
                     data += ",";
                 }else {
@@ -160,6 +182,10 @@ function exportKTP() {
         data += "\n";
         
     }
-    
+    let link = document.createElement('a');
+    link.download = 'KTP.csv';
+    var dataBlob = new Blob([data], {type: 'text/plain'});
+    link.href = window.URL.createObjectURL(dataBlob);
+    link.click();
     console.log(data);
 }
